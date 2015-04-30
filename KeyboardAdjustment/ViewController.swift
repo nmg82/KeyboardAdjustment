1ad38e41920bdfbc8e22c5915b3d3cb289616fc6
Nathan Gladson <nathan@egovtn.org>
2015-04-30 16:53:24 -0500

    Sample code that adjusts form when keyboard covers the element being edited.

--- KeyboardAdjustment/ViewController.swift
+++ KeyboardAdjustment/ViewController.swift
@@ -1,25 +1,54 @@
-//
-//  ViewController.swift
-//  KeyboardAdjustment
-//
-//  Created by Nathan Gladson on 4/30/15.
-//  Copyright (c) 2015 nmg82. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    var activeField: UITextField?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardDidShow:", name: UIKeyboardDidShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillBeHidden:", name: UIKeyboardWillHideNotification, object: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        activeField = textField
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        activeField = nil
+    }
+    
+    func keyboardDidShow(notification: NSNotification) {
+        let info = notification.userInfo!
+        var keyboardRect = info[UIKeyboardFrameBeginUserInfoKey]!.CGRectValue()
+        keyboardRect = view.convertRect(keyboardRect, fromView: nil)
+        
+        let padding = CGFloat(10)
+        let contentInsetHeight = keyboardRect.size.height + padding
+        
+        scrollView.contentInset = UIEdgeInsetsMake(0.0, 0.0, contentInsetHeight, 0.0)
+        
+        var rect = view.frame
+        rect.size.height -= keyboardRect.size.height
+        
+        if !CGRectContainsPoint(rect, activeField!.frame.origin) {
+            scrollView.scrollRectToVisible(activeField!.frame, animated: true)
+        }
+    }
+    
+    func keyboardWillBeHidden(notification: NSNotification) {
+        scrollView.contentInset = UIEdgeInsetsZero
+        scrollView.scrollIndicatorInsets = UIEdgeInsetsZero
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        view.endEditing(true)
+        return true
+    }
 }
-
